9a284b1e79ced71813d251f0c95b37470bf28f55
Adrian Miller <adrian.m.miller@gmail.com>
2023-12-23 05:15:33 +1100

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,5 @@
+git init --initial-branch=main
+git remote add origin https://gitlab.com/adrian.m.miller/coloursatmodifier.git
+git add .
+git commit -m "Initial commit"
+git push --set-upstream origin main
